bbfc7b508ae2261d6ca075c8e6ddc280b029e3c2
Josh Bivens <hotbioform@gmail.com>
2015-11-19 22:35:18 -0700

    Add acceleration

--- js/game.js
+++ js/game.js
@@ -12,6 +12,10 @@ var game = new Phaser.Game(
 
 var player, starfield, cursors;
 
+var ACCELERATION = 2000;
+var DRAG = 600;
+var MAXSPEED = 400;
+
 function preload() {
   game.load.image("starfield", "assets/img/starfield.png");
   game.load.image("ship", "assets/img/starship.png");
@@ -20,25 +24,30 @@ function preload() {
 function create() {
   starfield = game.add.tileSprite(0, 0, 750, 900, "starfield");
   player = game.add.sprite(375, 780, "ship");
+
   player.anchor.setTo(0.5, 0.5);
   game.physics.enable(player, Phaser.Physics.ARCADE);
+  player.body.maxVelocity.setTo(MAXSPEED, MAXSPEED);
+  player.body.drag.setTo(DRAG, DRAG);
+
   cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update() {
   starfield.tilePosition.y += 0.5;
 
-  player.body.velocity.setTo(0, 0);
+  player.body.acceleration.setTo(0, 0);
+
   if (cursors.left.isDown) {
-    player.body.velocity.x = -300;
+    player.body.acceleration.x = -ACCELERATION;
   } else if (cursors.right.isDown) {
-    player.body.velocity.x = 300;
+    player.body.acceleration.x = ACCELERATION;
   }
 
   if (cursors.up.isDown) {
-    player.body.velocity.y = -300;
+    player.body.acceleration.y = -ACCELERATION;
   } else if (cursors.down.isDown) {
-    player.body.velocity.y = 300;
+    player.body.acceleration.y = ACCELERATION;
   }
 }
 
