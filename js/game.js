5185097ecb71d1253fb0438b94bfac4d0a4257cb
Josh Bivens <hotbioform@gmail.com>
2015-11-19 22:14:45 -0700

    Add cursors and movement to player

--- js/game.js
+++ js/game.js
@@ -10,7 +10,7 @@ var game = new Phaser.Game(
   }
 );
 
-var player, starfield;
+var player, starfield, cursors;
 
 function preload() {
   game.load.image("starfield", "assets/img/starfield.png");
@@ -21,10 +21,25 @@ function create() {
   starfield = game.add.tileSprite(0, 0, 750, 1334, "starfield");
   player = game.add.sprite(375, 1180, "ship");
   player.anchor.setTo(0.5, 0.5);
+  game.physics.enable(player, Phaser.Physics.ARCADE);
+  cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update() {
   starfield.tilePosition.y += 0.5;
+
+  player.body.velocity.setTo(0, 0);
+  if (cursors.left.isDown) {
+    player.body.velocity.x = -300;
+  } else if (cursors.right.isDown) {
+    player.body.velocity.x = 300;
+  }
+
+  if (cursors.up.isDown) {
+    player.body.velocity.y = -300;
+  } else if (cursors.down.isDown) {
+    player.body.velocity.y = 300;
+  }
 }
 
 function render() {
